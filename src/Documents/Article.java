2339bfc681f90463a29a0997d4553d59eca6b56e
FabiDeG <fabiola.d.g.12@gmail.com>
2022-09-26 08:00:38 -0600

    Article base

--- src/Documents/Article.java
+++ src/Documents/Article.java
@@ -1,5 +1,15 @@
 package Documents;
 
-public class Article {
+public class Article extends Doc{
 
+	public Article(int[] _ID, String[] _Title, String[] _Author, String[] _Topic, String[] _Copy, String[] _Status){
+		super();
+		this.setID(_ID);
+		this.setTitle(_Title);
+		this.setAuthor(_Author);
+		this.setTopic(_Topic);
+		this.setCopy(_Copy);
+		this.setStatus(_Status);
+	}
+	
 }
