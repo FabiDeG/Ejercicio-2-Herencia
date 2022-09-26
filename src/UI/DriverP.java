b79a382937382267b0970879f394ed9fde6e3c73
FabiDeG <fabiola.d.g.12@gmail.com>
2022-09-26 10:33:44 -0600

    Menú

--- src/UI/DriverP.java
+++ src/UI/DriverP.java
@@ -1,5 +1,21 @@
 package UI;
+import java.util.Scanner;
 
 public class DriverP {
 
+	public static void MostrarMenu() {
+        System.out.println("Bienvenid@ a la biblioteca");
+        System.out.println("Por favor elige cual de las siguientes acciones desesas realizar");
+        System.out.println("1. Insertar un libro");
+        System.out.println("2. Según la ID, devolver un título de cualquier publicación");
+        System.out.println("3. Cantidad de documentos por materia");
+        System.out.println("4. Realizar un préstamo");
+        System.out.println("5. Realizar una devolución");
+        System.out.println("6. Cantidad de documentos que se ha prestado a un cliente según su tipo");
+        System.out.println("7. Cantidad de revistas de una materia en la biblioteca");
+        System.out.println("8. Determinar stock de libros o artículos");
+        System.out.println("\n");
+    }
+	
+	
 }
