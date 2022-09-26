f9b31e2e3db6029b6a0a37b1843ba55b44750cea
FabiDeG <fabiola.d.g.12@gmail.com>
2022-09-26 12:30:05 -0600

    Update DriverP.java

--- src/UI/DriverP.java
+++ src/UI/DriverP.java
@@ -1,8 +1,13 @@
 package UI;
 import java.util.Scanner;
+import Documents.Article;
+import Documents.Books;
+import Documents.Magazine;
+import People.Client;
 
 public class DriverP {
-
+	public DriverP() {
+    }
 	public static void MostrarMenu() {
         System.out.println("Bienvenid@ a la biblioteca");
         System.out.println("Por favor elige cual de las siguientes acciones desesas realizar");
@@ -14,8 +19,46 @@ public class DriverP {
         System.out.println("6. Cantidad de documentos que se ha prestado a un cliente según su tipo");
         System.out.println("7. Cantidad de revistas de una materia en la biblioteca");
         System.out.println("8. Determinar stock de libros o artículos");
+        System.out.println("9. Crear un usuario de la biblioteca nuevo");
+        System.out.println("10. Terminar el programa");
         System.out.println("\n");
     }
 	
+	public static void main(String[] args) {
+        Scanner escaner = new Scanner(System.in);
+        String opcion = "";
+        Books Libro = NewBook();
+        Magazine revista = NewMagazine();
+        Article articulo = NewArticle();
+        Client cliente = NewClient();
+        
+        while(!opcion.equals("10")) {
+            MostrarMenu();
+            opcion = "" + escaner.nextInt();
+            
+        }
+        
+	}
 	
+	public static Books NewBook() {
+        Books Libro = new Books(null, null, null, null, null, null, null);
+        return Libro;
+    }
+
+    public static Magazine NewMagazine() {
+    	Magazine revista = new Magazine(null, null, null, null, null);
+        return revista;
+    }
+    
+    public static Article NewArticle() {
+    	Article articulo = new Article(null, null, null, null, null, null);
+        return articulo;
+    }
+
+    public static Client NewClient() {
+    	Client cliente = new Client();
+        return cliente;
+    }
 }
+
+
