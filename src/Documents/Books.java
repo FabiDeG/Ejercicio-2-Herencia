12b108f5e27e1d1443a1d7f2f6ef5a183b086a29
FabiDeG <fabiola.d.g.12@gmail.com>
2022-09-26 07:44:50 -0600

    Segunda parte

---
+++ src/Documents/Books.java
@@ -0,0 +1,18 @@
+package Documents;
+
+public class Books extends Doc {
+
+	public Books(int[] _ID, String[] _Title, String[] _Author, String[] _Publisher, String[] _Topic, String[] _Copy, String[] _Status){
+		super();
+		this.setID(_ID);
+		this.setTitle(_Title);
+		this.setAuthor(_Author);
+		this.setPublisher(_Publisher);
+		this.setTopic(_Topic);
+		this.setCopy(_Copy);
+		this.setStatus(_Status);
+	}
+	
+
+	
+}
---
+++ src/Documents/Doc.java
@@ -0,0 +1,99 @@
+package Documents;
+
+import javax.swing.Spring;
+
+public abstract class Doc {
+	
+	protected int[] ID;
+	protected String[] Title;
+	protected String[] Author;
+	protected String[] Publisher ;
+	protected String[] Topic;
+	protected String[] Copy;
+	protected String[] Status;
+	
+	public void SortingAlgorithm(int[] _ID) {
+		this.setID(_ID);
+	}
+	
+	public int[] getID() {
+		return ID;
+	}
+	
+	public void setID(int[] ID) {
+		this.ID = ID;
+	}
+	
+	public void SortingAlgorithm1(String[] _Title) {
+		this.setTitle(_Title);
+	}
+	
+	public String[] getTitle() {
+		return Title;
+	}
+	
+	public void setTitle(String[] Title) {
+		this.Title = Title;
+	}
+	
+	public void SortingAlgorithm2(String[] _Author) {
+		this.setAuthor(_Author);
+	}
+	
+	public String[] getAuthor() {
+		return Author;
+	}
+	
+	public void setAuthor(String[] Author) {
+		this.Author = Author;
+	}
+	
+	public void SortingAlgorithm3(String[] _Publisher) {
+		this.setPublisher(_Publisher);
+	}
+	
+	public String[] getPublisher() {
+		return Publisher;
+	}
+	
+	public void setPublisher(String[] Publisher) {
+		this.Publisher = Publisher;
+	}
+	
+	public void SortingAlgorithm4(String[] _Topic) {
+		this.setTopic(_Topic);
+	}
+	
+	public String[] getTopic() {
+		return Topic;
+	}
+	
+	public void setTopic(String[] Topic) {
+		this.Topic = Topic;
+	}
+	
+	public void SortingAlgorithm5(String[] _Copy) {
+		this.setCopy(_Copy);
+	}
+	
+	public String[] getCopy() {
+		return Copy;
+	}
+	
+	public void setCopy(String[] Copy) {
+		this.Copy = Copy;
+	}
+	
+	public void SortingAlgorithm6(String[] _Status) {
+		this.setStatus(_Status);
+	}
+	
+	public String[] getStatus() {
+		return Status;
+	}
+	
+	public void setStatus(String[] Status) {
+		this.Status = Status;
+	}
+	
+}
